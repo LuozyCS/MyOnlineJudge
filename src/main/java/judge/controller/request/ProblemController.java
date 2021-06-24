8ebd2175a2a996bc275f6ce7c2310b502ca0b2a6
wangshuang66 <13610787240@qq.com>
2021-06-24 15:21:03 +0800

    样例为空的bug修复

--- src/main/java/judge/controller/request/ProblemController.java
+++ src/main/java/judge/controller/request/ProblemController.java
@@ -2,9 +2,15 @@ package judge.controller.request;
 
 import judge.controller.CookiendSession.CookieCheck;
 import judge.dataTransferObject.Comment;
+import judge.dataTransferObject.HistoryList;
+import judge.dataTransferObject.User;
+import judge.dataTransferObject.UserProblem;
+import judge.dataTransferObject.Example;
+import judge.mapper.ExampleMapper;
 import judge.mapper.ProblemMapper;
 import judge.mapper.CommentMapper;
 import judge.mapper.UserMapper;
+import judge.mapper.UserProblemMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class ProblemController
@@ -25,6 +31,8 @@ public class ProblemController
     @Autowired private ProblemMapper problemMapper;
     @Autowired private CommentMapper commentMapper;
     @Autowired private CookieCheck cookieCheck;
+    @Autowired private UserProblemMapper userProblemMapper;
+    @Autowired private ExampleMapper exampleMapper;
     @GetMapping("/problem/id={id}")
     public String problem(
             @PathVariable("id") int id
@@ -44,6 +52,13 @@ public class ProblemController
         for (Comment comment : directComments)
             comment.setPublisher(userMapper.getUserById(comment.getPublisher().getId()));
 
+        //接下来将要获得第一个样例输入以及输出
+        Example example1=exampleMapper.getFirstInputById(id);
+        model.addAttribute("inputExample",example1);
+        Example example2=exampleMapper.getFirstOutputById(id);
+        model.addAttribute("outputExample", example2);
+
+
 //        System.err.println(model.getAttribute("Comments"));
         return "site/problem-details";
     }
@@ -54,12 +69,22 @@ public class ProblemController
             ,Model model
     ){
         Cookie[] cookies = request.getCookies();
+        model = cookieCheck.check(cookies,model);
         if(cookieCheck.Admincheck(cookies)==false){//不是管理员就隐藏按钮
             model.addAttribute("failed","you are not administrator");
         }
+
         //在此添加排行榜与提交历史内容
-
-
+        User user=(User)model.getAttribute("User");
+        List<UserProblem> list = userProblemMapper.getAllByUserId(user.getId());
+        ArrayList<HistoryList> historyLists = new ArrayList<>(list.size());
+        for(UserProblem each: list){
+            HistoryList temp =new HistoryList();
+            temp.setUserProblem(each);
+            temp.setProblemTitle(problemMapper.getTitle(each.getProblem_id()));
+            historyLists.add(temp);
+        }
+        model.addAttribute("HistoryList",historyLists);
 
         return "admin/page_list";
     }
